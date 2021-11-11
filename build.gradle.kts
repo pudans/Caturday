709d84b316133cc2ee1fa6ca88fed99173cd4b38
kkarpyshev <kkarpyshev@ivi.ru>
2021-11-11 18:29:20 +0300

    version change

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,7 @@ buildscript {
 		mavenCentral()
 	}
 	dependencies {
-//		classpath(dependencyNotation = "com.android.tools.build:gradle:7.1.0-alpha13")
-		classpath("com.android.tools.build:gradle:7.0.1")
+		classpath(dependencyNotation = "com.android.tools.build:gradle:7.1.0-alpha03")
 		classpath(kotlin(module = "gradle-plugin", version = "1.5.31"))
 		classpath(dependencyNotation = "com.google.dagger:hilt-android-gradle-plugin:2.40")
 		classpath(dependencyNotation = "com.google.gms:google-services:4.3.10")
